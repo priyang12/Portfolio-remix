a4cfec291e6e8cc5418586357448a6ab93905a21
Priyang <patelpriyang95@gmail.com>
2022-12-30 21:44:30 +0530

    total Time to read

--- app/routes/Blogs/$Slug.tsx
+++ app/routes/Blogs/$Slug.tsx
@@ -5,6 +5,7 @@ import { useLoaderData } from "@remix-run/react";
 import { getMDXComponent } from "mdx-bundler/client";
 import { redirect } from "react-router";
 import { GetBlog } from "~/Utils/Mdx.server";
+import readTime from "~/Utils/readTime";
 
 export const loader = async ({ params }: LoaderArgs) => {
   const { Slug } = params;
@@ -37,6 +38,7 @@ export const meta: MetaFunction<typeof loader> = ({ params, data }) => {
 export default function Blogs() {
   const { frontmatter, code } = useLoaderData<typeof loader>();
 
+  const TotalTime = React.useCallback(() => readTime(code), []);
   const Component = React.useMemo(() => getMDXComponent(code), [code]);
 
   return (
@@ -49,6 +51,10 @@ export default function Blogs() {
           className="rounded-xl"
         />
       </figure>
+      <h1 className="mx-2xl my-sm text-2xl">
+        Total Read Time : {TotalTime()} Mins
+      </h1>
+
       <article className="prose m-auto p-5 prose-h1:text-primary prose-h2:text-primary prose-img:rounded-3xl md:p-0 lg:prose-xl">
         <Component />
       </article>
